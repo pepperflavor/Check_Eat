ab79c868e3c6778ee346b225ce790c42fcbac363
Hajin Choi <j8747j@gmail.com>
2025-07-11 03:13:58 +0900

    AAA

--- src/login-data/login-data.service.ts
+++ src/login-data/login-data.service.ts
@@ -1,12 +1,12 @@
 import { HttpStatus, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
 
-// 로그인시 공통 기능 부분 분리
+// 로그인, 회원가입시 공통 기능 부분 분리
 @Injectable()
 export class LoginDataService {
   constructor(private readonly prisma: PrismaService) {}
 
-  // 이메일 중복확인
+  //  이메일 중복확인
   async isExistEmail(id: string, user_grade: number) {
     const checkLogEmail = await this.prisma.loginData.findUnique({
       where: {
@@ -60,14 +60,42 @@ export class LoginDataService {
   }
 
   // 아이디 중복확인
-  async isExistID(id: string) {
-    const result = await this.prisma.loginData.findUnique({
+  async isExistID(id: string, user_grade: number) {
+    const idData = await this.prisma.loginData.findUnique({
       where: {
         ld_log_id: id,
       },
     });
 
-    if (result) {
+    if (idData && user_grade == 0 && idData.ld_user_id !== null) {
+      // 개인유저
+      const isExistUserID = await this.prisma.user.findUnique({
+        where: {
+          user_id: idData.ld_user_id,
+        },
+        select: {
+          user_id: true,
+        },
+      });
+
+      return isExistUserID
+        ? { message: '아이디가 존재합니다', status: HttpStatus.CONFLICT }
+        : { message: '아이디가 존재하지 않습니다', status: HttpStatus.OK };
+
+    } else if (idData && user_grade == 1 && idData.ld_sajang_id !== null) {
+      // 사장일때
+      const isExistSajangID = await this.prisma.sajang.findUnique({
+        where: {
+          sa_id: idData.ld_sajang_id,
+        },
+        select: {
+          sa_id: true,
+        },
+      });
+
+      return isExistSajangID
+        ? { message: '아이디가 존재합니다', status: HttpStatus.CONFLICT }
+        : { message: '아이디가 존재하지 않습니다', status: HttpStatus.OK };
     }
   }
 }
--- src/sajang/sajang.controller.ts
+++ src/sajang/sajang.controller.ts
@@ -5,9 +5,23 @@ import { SajangService } from './sajang.service';
 export class SajangController {
   constructor(private readonly sajangService: SajangService) {}
 
+  // 음식 등록
+  @Post('regist-food')
+  async registFood() {}
+
+  // 할랄 업장 인증
+  @Post('regist-halal')
+  async registHalal() {}
+
+  // 사업자 등록증 등록
+  @Post('regist-certification')
+  async registCertification() {
+    const result = await this.sajangService.checkBusinessRegistration();
+  }
+
   // 사업자 등록증 진위 여부 확인, post 로만 제공
   @Post('check-business-registration')
   async checkBusinessRegistration() {
-    const result = await this.sajangService.checkBusinessRegistration();
+    
   }
 }
