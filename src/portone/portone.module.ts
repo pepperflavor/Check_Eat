d123f9b0992c6d489460913c6158ed45c7aad227
pepperflavor <j8747j@gmail.com>
2025-08-12 18:23:12 +0900

    사장마이페이지

--- src/portone/portone.module.ts
+++ src/portone/portone.module.ts
@@ -1,10 +1,10 @@
-// import { Module } from '@nestjs/common';
-// import { PortoneService } from './portone.service';
-// import { PortoneController } from './portone.controller';
-// import { PrismaService } from 'src/prisma.service';
+import { Module } from '@nestjs/common';
+import { PortoneService } from './portone.service';
+import { PortoneController } from './portone.controller';
+import { PrismaService } from 'src/prisma.service';
 
-// @Module({
-//   controllers: [PortoneController],
-//   providers: [PortoneService, PrismaService],
-// })
-// export class PortoneModule {}
+@Module({
+  controllers: [PortoneController],
+  providers: [PortoneService, PrismaService],
+})
+export class PortoneModule {}
--- src/portone/portone.service.spec.ts
+++ src/portone/portone.service.spec.ts
@@ -1,18 +1,18 @@
-// import { Test, TestingModule } from '@nestjs/testing';
-// import { PortoneService } from './portone.service';
+import { Test, TestingModule } from '@nestjs/testing';
+import { PortoneService } from './portone.service';
 
-// describe('PortoneService', () => {
-//   let service: PortoneService;
+describe('PortoneService', () => {
+  let service: PortoneService;
 
-//   beforeEach(async () => {
-//     const module: TestingModule = await Test.createTestingModule({
-//       providers: [PortoneService],
-//     }).compile();
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [PortoneService],
+    }).compile();
 
-//     service = module.get<PortoneService>(PortoneService);
-//   });
+    service = module.get<PortoneService>(PortoneService);
+  });
 
-//   it('should be defined', () => {
-//     expect(service).toBeDefined();
-//   });
-// });
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
