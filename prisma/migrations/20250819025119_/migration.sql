97a70bffa924eb778dcf6d58e67d7ccfce3e5d7f
pepperflavor <j8747j@gmail.com>
2025-08-19 11:52:31 +0900

    schema

---
+++ prisma/migrations/20250819025119_/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `holi_sajang_id` on the `Holiday` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Holiday" DROP COLUMN "holi_sajang_id";
